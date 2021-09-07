cc25f6710ef3ed686b39deb95f50a37017564d64
Rick <kressins@gmail.com>
2021-09-07 17:38:30 -0500

    Update ClickButton.h
    added enum with no_pin option and update(btnState) override

--- ClickButton1/ClickButton.h
+++ ClickButton1/ClickButton.h
@@ -10,19 +10,23 @@
 
 #define CLICKBTN_PULLUP HIGH
 
+//example: ClickButton button1(PIN_SWITCH, HIGH, cb_pinMode::cb_INPUT_PULLDOWN);
+
+enum cb_pinMode { cb_INPUT = INPUT, cb_INPUT_PULLUP = INPUT_PULLUP, cb_INPUT_PULLDOWN = INPUT_PULLDOWN, cb_NO_PIN };
 
 class ClickButton
 {
   public:
     ClickButton(uint8_t buttonPin);
     ClickButton(uint8_t buttonPin, boolean active);
-    ClickButton(uint8_t buttonPin, boolean active, boolean internalPullup);
+    ClickButton(uint8_t buttonPin, boolean active, cb_pinMode pinMode);
     void Update();
+    void Update(boolean btnState);
     int clicks;                   // button click counts to return
     boolean depressed;            // the currently debounced button (press) state (presumably it is not sad :)
-    long debounceTime;
-    long multiclickTime;
-    long longClickTime;
+    unsigned long debounceTime;
+    unsigned long multiclickTime;
+    unsigned long longClickTime;
 	boolean inProcess;
   private:
     uint8_t _pin;                 // Arduino pin connected to the button
@@ -30,7 +34,7 @@ class ClickButton
     boolean _btnState;            // Current appearant button state
     boolean _lastState;           // previous button reading
     int _clickCount;              // Number of button clicks within multiclickTime milliseconds
-    long _lastBounceTime;         // the last time the button input pin was toggled, due to noise or a press
+    unsigned long _lastBounceTime;         // the last time the button input pin was toggled, due to noise or a press
 };
 
 #endif
