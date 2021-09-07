5b3e54653a32a072cd2d19e237d5d7c67a9af0c1
Rick <kressins@gmail.com>
2021-09-07 10:24:37 -0500

    Update test.ino

--- test.ino
+++ test.ino
@@ -2,22 +2,28 @@
 #include "Adafruit_FreeTouch.h"
 #include "HID-Project.h"  // https://github.com/NicoHood/HID
 #include "ClickButton.h"
+#include <FlashStorage.h>
 
 ClickButton button1(PIN_SWITCH, HIGH, LOW);
-
 // Create the neopixel strip with the built in definitions NUM_NEOPIXEL and PIN_NEOPIXEL
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_NEOPIXEL, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
-
 // Create the touch pad
 Adafruit_FreeTouch qt = Adafruit_FreeTouch(PIN_TOUCH, OVERSAMPLE_4, RESISTOR_50K, FREQ_MODE_NONE);
-
 int16_t neo_brightness = 20; // initialize with 20 brightness (out of 255)
 
+typedef struct {
+  char stuff[100];
+} flashData;
+FlashStorage(my_flash_store, flashData);
+flashData fd;
+
+int writeStop = -1;
+
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   button1.debounceTime   = 25;   // Debounce timer in ms
   button1.multiclickTime = 300;  // Time limit for multi clicks
-  button1.longClickTime  = 800; // time until "held-down clicks" register
+  button1.longClickTime  = 700; // time until "held-down clicks" register
   
   pinMode(PIN_SWITCH, INPUT_PULLDOWN);
   //while (!Serial);
@@ -29,12 +35,14 @@ void setup() {
   //  Serial.println("Failed to begin qt");
   Keyboard.begin();
   Mouse.begin();
+  fd = my_flash_store.read();
 }
 
 unsigned long lp=0;
 unsigned long lastMouse=0;
 bool prnt = false;
 bool wiggle = false;
+bool prog = false;
 
 void loop() {
   
@@ -49,22 +57,26 @@ void loop() {
   button1.Update();
   switch (button1.clicks) {
     case 1:
+      prog = false;
       break;
     case 2:
       wiggle = !wiggle;
       break;
     case -1:
-      Keyboard.println(" ");
+      Keyboard.println(fd.stuff);
       break;
     case -2:
       Keyboard.press(KEY_LEFT_CTRL);
       delay(100);
       Keyboard.releaseAll();
-      delay(1000);
-      Keyboard.println(" ");
+      delay(500);
+      Keyboard.println(fd.stuff);
+      break;
+    case -5:
+      prog = true;
+      writeStop = -1;
       break;
     default:
-      // statements
       break;
   }
   //if (button1.clicks != 0) {
@@ -73,27 +85,50 @@ void loop() {
 
   // measure the captouches
   uint16_t touch = qt.measure();
-  int incomingByte = 0;
-  if (wiggle) {
 
-    //use :echo asd > COM6
-    while (Serial.available() > 0) {
-      incomingByte = Serial.read();
-      wiggle=false;
-    
+  int incomingByte;
+  if (prog){
+    //use echo asd>COM6
+    if (Serial.available() > 0) {
+      while (Serial.available() > 0) {
+        incomingByte = Serial.read();
+        if(incomingByte==10){
+          //flush serial
+          delay(10);
+          while (Serial.available() > 0) {
+            incomingByte = Serial.read();
+          }
+          prog = false;
+          writeStop = -1;
+          fd.stuff[writeStop] = 0;
+          my_flash_store.write(fd);
+        }else{
+          writeStop++;
+          fd.stuff[writeStop] = incomingByte;
+        } 
+      }
     }
-      
-    strip.setBrightness(neo_brightness);
+  }
+  
+  if (wiggle) {
     if(millis()-lastMouse > 300000){
       Mouse.move(1, 0, 0);
       delay(10);
       Mouse.move(-1, 0, 0);
       lastMouse = millis();
     }
-  } else {
-    strip.setBrightness(0);
   }
 
-  strip.setPixelColor(0, strip.Color(0, 0, 255));
+  //set the noepixel color
+  if(prog){
+    strip.setPixelColor(0, strip.Color(255, 0, 0));    
+    strip.setBrightness(neo_brightness);
+  }else if(wiggle) {
+    strip.setPixelColor(0, strip.Color(0, 0, 255));
+    strip.setBrightness(neo_brightness);    
+  }else{
+    strip.setBrightness(0);    
+  }
+
   strip.show();
 }
